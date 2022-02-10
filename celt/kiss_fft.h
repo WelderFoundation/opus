f75420629d9100d2148565da6fec33ca22d25ffd
Robert Campbell <waprave@gmail.com>
2022-02-10 16:58:55 -0500

    Removed intrinsics for Emscripten.

--- celt/kiss_fft.h
+++ celt/kiss_fft.h
@@ -38,6 +38,15 @@
 extern "C" {
 #endif
 
+//+WELDER
+// Emscripten doesn't have intrinsics.
+#ifdef EMSCRIPTEN
+#ifdef USE_SIMD
+#undef USE_SIMD
+#endif
+#endif
+//-WELDER
+
 #ifdef USE_SIMD
 # include <xmmintrin.h>
 # define kiss_fft_scalar __m128
